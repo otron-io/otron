341473fab0650badefb8b68a38916a47a34b1e22
Matthew Blode <m@blode.co>
2025-04-25 16:32:27 +1000

    Vercel

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  return res.status(200).json({ message: "Fingertip Bot is running!" });
+  return res.status(200).json({ message: "Fingertip Agent is running!" });
 }
